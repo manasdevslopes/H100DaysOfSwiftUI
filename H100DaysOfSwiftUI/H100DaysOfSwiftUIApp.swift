7e1dda3765fc8d21c268b8322c6c4955f4f11018
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-01-24 17:15:23 +0530

    Initial Commit

---
+++ H100DaysOfSwiftUI/H100DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H100DaysOfSwiftUIApp.swift
+//  H100DaysOfSwiftUI
+//
+//  Created by MANAS VIJAYWARGIYA on 24/01/21.
+//
+
+import SwiftUI
+
+@main
+struct H100DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
