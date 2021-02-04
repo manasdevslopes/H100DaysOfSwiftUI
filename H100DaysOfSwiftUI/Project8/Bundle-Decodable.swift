8896142e7207997f6f6a1a588eca83cc978bb118
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-02-04 22:59:26 +0530

    Day 42 Completed - Codable and showing data from 2 json

--- H100DaysOfSwiftUI/Project8/Bundle-Decodable.swift
+++ H100DaysOfSwiftUI/Project8/Bundle-Decodable.swift
@@ -19,6 +19,11 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
+        //For formatting the date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
         guard let loaded = try? decoder.decode(T.self, from: data) else{
             fatalError("Failed to decode \(file) from bundle.")
         }
--- H100DaysOfSwiftUI/Project8/Moonshot.swift
+++ H100DaysOfSwiftUI/Project8/Moonshot.swift
@@ -10,12 +10,29 @@ import SwiftUI
 
 struct Moonshot: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
-    let mission: [Mission] = Bundle.main.decode("missions.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        VStack {
-            Text("\(astronauts.count)")
-            Text("\(mission[0].crew[0].role)")
+//        VStack {
+//            Text("\(astronauts.count)")
+//            Text("\(missions[0].crew[0].role)")
+//        }
+        NavigationView {
+            List(missions){mission in
+                NavigationLink(destination: MissionView(mission:mission, astronauts: self.astronauts) ) {
+                    Image(mission.image)
+                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    VStack(alignment:.leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
         }
     }
 }
